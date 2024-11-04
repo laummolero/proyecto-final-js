4bc17c2b80012a3c011d4ef9066bc400cc7987d9
laury mosquera <laury.25.mosquera@gmail.com>
2024-11-03 21:55:52 -0300

    eliminacion de server y vuelta a localstorage

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
-const API_URL =
-  "https://6727e4c3270bd0b97553d39a.mockapi.io/inventory-api/v1/inventory";
-
-//funcion para cargar el inventario desde el server
-async function loadInventory() {
-  let response = await fetch(API_URL);
-  let inventory = await response.json();
+//funcion para cargar el inventario desde el localstorage
+function loadInventory() {
+  let inventory = JSON.parse(localStorage.getItem("inventory")) || [];
   let inventoryList = document.querySelector("#inventoryList");
   inventoryList.innerHTML = ""; //evita que se los items agregados anteriormente se vuelvan a repetir al ingresar un nuevo item
 
-  inventory.forEach((item) => {
+  inventory.forEach((item, index) => {
     let li = document.createElement("li");
-    li.innerHTML = `Number: ${item.number}, Name: ${item.name}, Added: ${item.timestamp} <button onclick="deleteItem(${item.id})">Delete</button>`;
+    li.innerHTML = `Number: ${item.number}, Name: ${item.name}, Added: ${item.timestamp} <button onclick="deleteItem(${index})">Delete</button>`;
     inventoryList.appendChild(li);
   });
 }
 
-//funcion para agregar un item al servidor
-async function addItem() {
+//funcion para agregar un item al inventario
+function addItem() {
   let itemNumber = document.querySelector("#itemNumber").value;
   let itemName = document.querySelector("#itemName").value;
   let messageDiv = document.querySelector("#message");
@@ -26,48 +22,35 @@ async function addItem() {
     Swal.fire("Error", "Please, complete both boxes", "error");
     return;
   }
-
-  let DateTime = luxon.DateTime; //uso de Luxon para añadir fecha y hora
-  let timestamp = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
-
-  let newItem = { number: itemNumber, name: itemName, timestamp: timestamp };
-
-  let response = await fetch(API_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(newItem),
-  });
-
-  //mensaje en console
-  if (response.ok) {
-    console.log(
-      `Item added: Number: ${itemNumber}, Name: ${itemName}, Timestamp: ${timestamp}`
-    );
-    messageDiv.textContent = "Item added successfully.";
-    loadInventory();
-  } else {
-    messageDiv.textContent = "Failed to add item.";
-  }
-
-  //limpiar las cajas
-  document.querySelector("#itemNumber").value = "";
-  document.querySelector("#itemName").value = "";
 }
 
-//funcion para eliminar los item del servidor
-async function deleteItem(id) {
-  let response = await fetch(`${API_URL}/${id}`, {
-    method: "DELETE",
-  });
+let inventory = JSON.parse(localStorage.getItem("inventory")) || [];
+let DateTime = luxon.DateTime; //uso de Luxon para añadir fecha y hora
+let timestamp = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
 
-  if (response.ok) {
-    console.log("Item removed.");
-    Swal.fire("Success", "Item removed successfully", "success");
-    loadInventory();
-  } else {
-    console.log("Failed to remove item");
-    Swal.fire("Error", "Failed to remove item", "error");
-  }
+inventory.push({ number: itemNumber, name: itemName, timestamp: timestamp });
+localStorage.setItem("inventory", JSON.stringify(inventory));
+
+//mensaje en console
+console.log(
+  `Item added: Number: ${itemNumber}, Name: ${itemName}, Timestamp: ${timestamp}`
+);
+messageDiv.textContent = "Item added successfully.";
+loadInventory();
+
+//limpiar las cajas
+document.querySelector("#itemNumber").value = "";
+document.querySelector("#itemName").value = "";
+
+//funcion para eliminar los item del inventario
+function deleteItem(index) {
+  let inventory = JSON.parse(localStorage.getItem("inventory")) || [];
+  inventory.splice(index, 1); //elimina el item en el index especificado
+  localStorage.setItem("inventory", JSON.stringify(inventory));
+
+  console.log("Item removed.");
+  Swal.fire("Success", "Item removed successfully", "success");
+  loadInventory();
 }
 
 //evento para el boton
